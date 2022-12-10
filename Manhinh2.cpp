28f979a83d8c61a149db25f71f5079609a60860d
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-11 00:18:36 +0700

    Create Manhinh2.cpp

---
+++ Manhinh2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	printf("\n\n\t\t(a) Press 0 to Exit.");
+	printf("\n\t\t(b) Press 1 to Login as admin.");
+	printf("\n\t\t(c) Press 2 to Register as admin.");
+	printf("\n\t\t(d) Press 3 to Login as Passenger.");
+	printf("\n\t\t(e) Press 4 to Register as Passenger.");
+	printf("\n\t\t(f) Press 5 to Display the User Manual.");
+	printf("\n\t\tEnter the desired option:    \t4");
+	printf("\n\n");
+	printf(" .o88b. db    db .d8888. d888888b  .d88b.  .88b  d88. d88888b d8888b.      .d8888. d888888b  d888b  d8b   db db    db d8888b.\n");
+	printf("d8P  Y8 88    88 88'  YP `~~88~~' .8P  Y8. 88'YbdP`88 88'     88  `8D      88'  YP   `88'   88' Y8b 888o  88 88    88 88  `8D\n");
+	printf("8P      88    88 `8bo.      88    88    88 88  88  88 88ooooo 88oobY'      `8bo.      88    88      88V8o 88 88    88 88oodD'\n");
+	printf("8b      88    88   `Y8b.    88    88    88 88  88  88 88~~~~~ 88`8b          `Y8b.    88    88  ooo 88 V8o88 88    88 88~~~  \n");
+	printf("Y8b  d8 88b  d88 db   8D    88    `8b  d8' 88  88  88 88.     88 `88.      db   8D   .88.   88. ~8~ 88  V888 88b  d88 88     \n");
+	printf(" `Y88P' ~Y8888P' `8888Y'    YP     `Y88P'  YP  YP  YP Y88888P 88   YD      `8888Y' Y888888P  Y888P  VP   V8P ~Y8888P' 88     \n");
+	printf("\n\n\n%60s ++++++++++++++ Welcome to the Customer Registration Portal ++++++++++++++", "");
+    printf("\nEnter your name :\tSalman");
+    printf("\nEnter your email address :\tsomeone@gmail.com");
+    printf("\nEnter your Password :\thithere123");
+    printf("\nEnter your Phone number :\t032890383");
+    printf("\nEnter your address :\tPakistan");
+    printf("\nEnter your age :\t19");
+    
+}
+
