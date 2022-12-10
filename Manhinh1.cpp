117cf7c9c02b90bd3d7e3b52aabad9767045d94c
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-10 14:56:26 +0700

    Create Manhinh1.cpp

---
+++ Manhinh1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+	printf("\n\n");
+	printf("888       888          888                                                888                   888888b.          d8888 888     888              d8888 d8b         888 d8b\n");
+	printf("888   o   888          888                                                888                   888   88b        d88888 888     888             d88888 Y8P         888 Y8P\n");
+	printf("888  d8b  888          888                                                888                   888  .88P       d88P888 888     888            d88P888             888\n");
+	printf("888 d888b 888  .d88b.  888  .d8888b  .d88b.  88888b.d88b.   .d88b.        888888  .d88b.        8888888K.      d88P 888 Y88b   d88P           d88P 888 888 888d888 888 888 88888b.   .d88b.  .d8888b\n");
+	printf("888d88888b888 d8P  Y8b 888 d88P'    d88''88b 888  888  88b d8P  Y8b       888    d88  88b       888   Y88b    d88P  888  Y88b d88P           d88P  888 888 888P    888 888 888  88b d8P  Y8b 88K\n");
+	printf("88888P Y88888 88888888 888 888      888  888 888  888  888 88888888       888    888  888       888    888   d88P   888   Y88o88P           d88P   888 888 888     888 888 888  888 88888888 .Y8888b.\n");
+	printf("8888P   Y8888 Y8b.     888 Y88b.    Y88..88P 888  888  888 Y8b.           Y88b.  Y88..88P       888   d88P  d8888888888    Y888P           d8888888888 888 888     888 888 888  888 Y8b.         X88\n");
+	printf("888P     Y888  'Y8888  888  'Y8888P  'Y88P'  888  888  888  'Y8888         'Y888  'Y88P'        8888888P'  d88P     888     Y8P           d88P     888 888 888     888 888 888  888  'Y8888   88888P.\n");
+	
+	
+	printf("\n\n\t\t(a) Press 0 to Exit.");
+	printf("\n\t\t(b) Press 1 to Login as admin.");
+	printf("\n\t\t(c) Press 2 to Register as admin.");
+	printf("\n\t\t(d) Press 3 to Login as Passenger.");
+	printf("\n\t\t(f) Press 5 to Display the User Manual.");
+	printf("\n\t\t(e) Press 4 to Register as Passenger.");
+	printf("\n\t\tEnter the desired option:    ");
+    
+}
+
