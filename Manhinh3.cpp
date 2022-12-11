973ca97b3ac1692420e32ba8b6d6495b36471227
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-12 00:12:14 +0700

    Create Manhinh3.cpp

---
+++ Manhinh3.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+	printf("\n\n\t\t(a) Press 0 to Exit.");
+	printf("\n\t\t(b) Press 1 to Login as admin.");
+	printf("\n\t\t(c) Press 2 to Register as admin.");
+	printf("\n\t\t(d) Press 3 to Login as Passenger.");
+	printf("\n\t\t(e) Press 4 to Register as Passenger.");
+	printf("\n\t\t(f) Press 5 to Display the User Manual.");
+	printf("\n\t\tEnter the desired option:    \t3");
+	printf("\n\n");
+	
+	printf(" .o88b. db    db .d8888. d888888b  .d88b.  .88b  d88. d88888b d8888b.      db       .d88b.   d888b  d888888b d8b   db\n");
+	printf("d8P  Y8 88    88 88'  YP `~~88~~' .8P  Y8. 88'YbdP`88 88'     88  `8D      88      .8P  Y8. 88' Y8b   `88'   888o  88\n");
+	printf("8P      88    88 `8bo.      88    88    88 88  88  88 88ooooo 88oobY'      88      88    88 88         88    88V8o 88\n");
+	printf("8b      88    88   `Y8b.    88    88    88 88  88  88 88~~~~~ 88`8b        88      88    88 88  ooo    88    88 V8o88\n");
+	printf("Y8b  d8 88b  d88 db   8D    88    `8b  d8' 88  88  88 88.     88 `88.      88booo. `8b  d8' 88. ~8~   .88.   88  V888\n");
+	printf(" `Y88P' ~Y8888P' `8888Y'    YP     `Y88P'  YP  YP  YP Y88888P 88   YD      Y88888P  `Y88P'   Y888P  Y888888P VP   V8P\n");
+	
+	printf("\n\n\nEnter the Email to Login :\tsomeone@gmail.com");
+	printf("\nEnter the Password :\thithere123");
+	
+	printf("\n\n\t\t Logged in Successfully as \"someone@gmail.com\"..... For further Proceedings, enter a value from below....");
+	printf("\n\n\t\t\t\t\t+++++++++ 3rd Layer Menu +++++++++\t\t\tLogged in as \"someone@gmail.com\"\n");
+	
+    printf("%-40s (a) Enter 1 to Book a flight....\n", "");
+    printf("%-40s (b) Enter 2 to update your Data....\n", "");
+    printf("%-40s (c) Enter 3 to delete your account....\n", "");
+    printf("%-40s (d) Enter 4 to Display Flight Schedule....\n", "");
+    printf("%-40s (e) Enter 5 to Cancel a Flight....\n", "");
+    printf("%-40s (f) Enter 6 to Display all flights registered by \"someone@gmail.com\"....\n", "");
+    printf("%-40s (g) Enter 0 to Go back to the Main Menu/Logout....\n", "");
+    printf("Enter the desired Choice :   ");
+}
+
