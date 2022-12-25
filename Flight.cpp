4676d26f3e1749b6020c4e1554edf87c9eb20eaf
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-25 09:24:34 +0700

    Update Flight.cpp

--- Flight.cpp
+++ Flight.cpp
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
 int main(){
-	
+	string flightSchedule;
+    string flightNumber;
+    string fromWhichCity;
+    string gate;
+    final String toWhichCity;
+    double distanceInMiles;
+    double distanceInKm;
+    string flightTime;
+    int numOfSeatsInTheFlight;
+    int customerIndex;
+    int nextFlightDay = 0;
 }
