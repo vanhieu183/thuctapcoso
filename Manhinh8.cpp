d5367aad99af3ee11e5240f1740dadcc7e54bc16
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 14:53:49 +0700

    Create Manhinh8.cpp

---
+++ Manhinh8.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("\t\t\t\t\t\t Enter 0 to Go back to the Main Menu/Logout....\n");
+	printf("Enter the desired Choice:	6\n");
+	printf("d8888b. d88888b  d888b  d888888b .d8888. d888888b d88888b d8888b. d88888b d8888b.      d88888b db      d888888b  d888b  db   db d888888b .d8888.     \n");
+	printf("88  `8D 88'     88' Y8b   `88'   88'  YP `~~88~~' 88'     88  `8D 88'     88  `8D      88'     88        `88'   88' Y8b 88   88 `~~88~~' 88'  YP     \n");
+	printf("88oobY' 88ooooo 88         88    `8bo.      88    88ooooo 88oobY' 88ooooo 88   88      88ooo   88         88    88      88ooo88    88    `8bo.       \n");
+	printf("88`8b   88~~~~~ 88  ooo    88      `Y8b.    88    88~~~~~ 88`8b   88~~~~~ 88   88      88~~~   88         88    88  ooo 88~~~88    88      `Y8b.     \n");
+	printf("88 `88. 88.     88. ~8~   .88.   db   8D    88    88.     88 `88. 88.     88  .8D      88      88booo.   .88.   88. ~8~ 88   88    88    db   8D     \n");
+	printf("88   YD Y88888P  Y888P  Y888888P `8888Y'    YP    Y88888P 88   YD Y88888P Y8888D'      YP      Y88888P Y888888P  Y888P  YP   YP    YP    `8888Y'     \n");
+	
+	printf("\nd8888b. db    db      d8888b.  .d8b.  .d8888. .d8888. d88888b d8b   db  d888b  d88888b d8888b.\n");
+	printf("88  `8D `8b  d8'      88  `8D d8' `8b 88'  YP 88'  YP 88'     888o  88 88' Y8b 88'     88  `8D\n");
+	printf("88oooY'  `8bd8'       88oodD' 88ooo88 `8bo.   `8bo.   88ooooo 88V8o 88 88      88ooooo 88oobY'\n");
+	printf("88~~~b.    88         88~~~   88~~~88   `Y8b.   `Y8b. 88~~~~~ 88 V8o88 88  ooo 88~~~~~ 88`8b\n");
+	printf("88   8D    88         88      88   88 db   8D db   8D 88.     88  V888 88. ~8~ 88.     88 `88.\n");
+	printf("Y8888P'    YP         88      YP   YP `8888Y' `8888Y' Y88888P VP   V8P  Y888P  Y88888P 88   YD\n");
+	
+	printf("\n\n\n%30s ++++++++++++++ Welcome to the Customer Registration Portal ++++++++++++++\n", "");
+	printf("Enter your name :	Salman\n");
+	printf("Enter your email address :	someone@gmail.com\n");
+	printf("Enter your Password :	hithere123\n");
+	printf("Enter your Phone number :		032890383\n");
+	printf("Enter your address :	Pakistan\n");
+	printf("Enter your age :	19\n");
+}	
