089925d0b7666781f20c917b8d0fe4b8f1d50ad2
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 14:46:25 +0700

    Create Manhinh6.cpp

---
+++ Manhinh6.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("\n\t\t\t\ts+++++++++ 3rd Layer Menu +++++++++				Logged in as \n");
+	printf("%-40s (a) Enter 1 to Book a flight....\n", "");
+    printf("%-40s (b) Enter 2 to update your Data....\n", "");
+    printf("%-40s (c) Enter 3 to delete your account....\n", "");
+    printf("%-40s (d) Enter 4 to Display Flight Schedule....\n", "");
+    printf("%-40s (e) Enter 5 to Cancel a Flight....\n", "");
+    printf("%-40s (f) Enter 6 to Display all flights registered by someone@gmail.com......\n","");
+    printf("%-40s (g) Enter 0 to Go back to the Main Menu/Logout....\n", "");
+    printf("\nEnter the desired Choice:	3\n");
+    
+    printf("d8888b. d88888b db      d88888b d888888b d88888b       .d8b.   .o88b.  .o88b.  .d88b.  db    db d8b   db d888888b\n");
+    printf("88  `8D 88'     88      88'     `~~88~~' 88'          d8' `8b d8P  Y8 d8P  Y8 .8P  Y8. 88    88 888o  88 `~~88~~'\n");
+    printf("88   88 88ooooo 88      88ooooo    88    88ooooo      88ooo88 8P      8P      88    88 88    88 88V8o 88    88   \n");
+    printf("88   88 88~~~~~ 88      88~~~~~    88    88~~~~~      88~~~88 8b      8b      88    88 88    88 88 V8o88    88   \n");
+    printf("88  .8D 88.     88booo. 88.        88    88.          88   88 Y8b  d8 Y8b  d8 `8b  d8' 88b  d88 88  V888    88   \n");
+    printf("Y8888D' Y88888P Y88888P Y88888P    YP    Y88888P      YP   YP  `Y88P'  `Y88P'  `Y88P'  ~Y8888P' VP   V8P    YP   \n");
+}
