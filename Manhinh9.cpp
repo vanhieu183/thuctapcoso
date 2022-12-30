d7935b6d336656a15a1b2b6774fbab9baa18809c
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 15:04:15 +0700

    Create Manhinh9.cpp

---
+++ Manhinh9.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("\n\t\tEnter the desired option:	2\n");
+	printf("\n .d8b.  d8888b. .88b  d88. d888888b d8b   db      .d8888. d888888b  d888b  d8b   db db    db d8888b.\n");
+	printf("d8' `8b 88  `8D 88'YbdP`88   `88'   888o  88      88'  YP   `88'   88' Y8b 888o  88 88    88 88  `8D\n");
+	printf("88ooo88 88   88 88  88  88    88    88V8o 88      `8bo.      88    88      88V8o 88 88    88 88oodD'\n");
+	printf("88~~~88 88   88 88  88  88    88    88 V8o88        `Y8b.    88    88  ooo 88 V8o88 88    88 88~~~  \n");
+	printf("88   88 88  .8D 88  88  88   .88.   88  V888      db   8D   .88.   88. ~8~ 88  V888 88b  d88 88     \n");
+	printf("YP   YP Y8888D' YP  YP  YP Y888888P VP   V8P      `8888Y' Y888888P  Y888P  VP   V8P ~Y8888P' 88     \n");
+	printf("\n\n");
+	printf("\nEnter the UserName to Register :	admin");
+	printf("\nEnter the Password to Register :	admin\n");
+	
+	printf(" 		(a) Press 0 to Exit.\n");
+	printf("		(b) Press 1 to Login as admin.\n");
+	printf("		(c) Press 2 to Register as admin.\n");
+	printf("		(d) Press 3 to Login as Passenger.\n");
+	printf("		(e) Press 4 to Register as Passenger.\n");
+	printf("		(f) Press 5 to Display the User Manual.\n");
+	printf("		Enter the desired option:     1\n");
+	
+	printf("\n .d8b.  d8888b. .88b  d88. d888888b d8b   db      db       .d88b.   d888b  d888888b d8b   db\n");
+	printf("d8' `8b 88  `8D 88'YbdP`88   `88'   888o  88      88      .8P  Y8. 88' Y8b   `88'   888o  88\n");
+	printf("88ooo88 88   88 88  88  88    88    88V8o 88      88      88    88 88         88    88V8o 88\n");
+	printf("88~~~88 88   88 88  88  88    88    88 V8o88      88      88    88 88  ooo    88    88 V8o88\n");
+	printf("88   88 88  .8D 88  88  88   .88.   88  V888      88booo. `8b  d8' 88. ~8~   .88.   88  V888\n");
+	printf("YP   YP Y8888D' YP  YP  YP Y888888P VP   V8P      Y88888P  `Y88P'   Y888P  Y888888P VP   V8P\n");
+	
+	printf("\n\n");
+	printf("\nEnter the UserName to login to the Management System :     admin\n");
+	printf("Enter the Password to login to the Management System :    admin\n");
+	
+	printf("%-20sLogged in Successfully as ..... For further Proceedings, enter a value from below....\n", "");
+	printf("\n\n%30s\t\t+++++++++ 2nd Layer Menu +++++++++					Logged in as \"admin\"\n", "");
+	printf("%-30s (a) Enter 1 to add new Passenger....\n", "");
+    printf("%-30s (b) Enter 2 to search a Passenger....\n", "");
+    printf("%-30s (c) Enter 3 to update the Data of the Passenger....\n", "");
+    printf("%-30s (d) Enter 4 to delete a Passenger....\n", "");
+    printf("%-30s (e) Enter 5 to Display all Passengers....\n", "");
+  	printf("%-30s (f) Enter 6 to Display all flights registered by a Passenger...\n", "");
+    printf("%-30s (g) Enter 7 to Display all registered Passengers in a Flight....\n", "");
+    printf("%-30s (h) Enter 8 to Delete a Flight....\n", "");
+    printf("%-30s (i) Enter 0 to Go back to the Main Menu/Logout....\n", "");
+    
+}
