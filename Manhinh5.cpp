7270098191706c73d59de33e3151516debbbffb2
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 14:43:38 +0700

    Create Manhinh5.cpp

---
+++ Manhinh5.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("\n\t\t\t\t+++++++++ 3rd Layer Menu +++++++++				Logged in as \n");
+	printf("%-40s (a) Enter 1 to Book a flight....\n", "");
+    printf("%-40s (b) Enter 2 to update your Data....\n", "");
+    printf("%-40s (c) Enter 3 to delete your account....\n", "");
+    printf("%-40s (d) Enter 4 to Display Flight Schedule....\n", "");
+    printf("%-40s (e) Enter 5 to Cancel a Flight....\n", "");
+    printf("%-40s (f) Enter 6 to Display all flights registered by ....\n","");
+    printf("%-40s (g) Enter 0 to Go back to the Main Menu/Logout....\n", "");
+    printf("%-40s Enter the desired Choice :   ","");
+	
+	printf("\nEnter the desired Choice:  2\n");
+	printf("\nd88888b d8888b. d888888b d888888b      d888888b d8b   db d88888b  .d88b. \n");
+	printf(  "88'     88  `8D   `88'   `~~88~~'        `88'   888o  88 88'     .8P  Y8.\n");
+	printf(  "88ooooo 88   88    88       88            88    88V8o 88 88ooo   88    88\n");
+	printf(  "88~~~~~ 88   88    88       88            88    88 V8o88 88~~~   88    88\n");
+	printf(  "88.     88  .8D   .88.      88           .88.   88  V888 88      `8b  d8'\n");
+	printf(  "Y88888P Y8888D' Y888888P    YP         Y888888P VP   V8P YP       `Y88P' \n");
+	
+	printf("\nEnter the new name of the Passenger:	David\n");
+	printf("Enter the new email address of Passenger David: someone#gmail.com\n");
+	printf("Enter the new Phone number of Passenger David: 932093-2323\n");
+	printf("Enter the new address of Passenger David:	USA\n");
+	printf("Enter the new age of Passenger David:	20\n");
+	printf("\n+------------+------------+-------------------------+---------+-------------------+---------------+-----------------+\n");
+	printf("| SerialNum  |   UserID   | Passenger Names         | Age     | EmailID\t\t  | Home Address  | Phone Number    |\n");
+    printf("+------------+------------+-------------------------+---------+-------------------+---------------+-----------------+\n");
+    printf("| 1	     |   350 717  | David	            | 20      | someone#gmail.com | USA		  | 932093-2323	    |\n");
+    printf("+------------+------------+-------------------------+---------+-------------------+---------------+-----------------+\n");
+}
