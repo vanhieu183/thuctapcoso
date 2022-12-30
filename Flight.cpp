d606b4f07f91f14af36ca1f5547ab272ca60cb23
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 15:48:24 +0700

    Update Flight.cpp

--- Flight.cpp
+++ Flight.cpp
@@ -6,20 +6,37 @@ struct Flight{
         char toWhichCity[50];
         char fromWhichCity[50];
         char gate[10];
-};
+	};
 void flightScheduler() {
     
-}
+	}
 
 void addNewCustomerToFlight() {
     
-}
+	}
+
+void addTicketsToExistingCustomer() {
+        
+	}
+
+void deleteFlight() {
+        
+    }
+
 int main(){
-	
+	int k;
     double distanceInMiles;
     double distanceInKm;
     char flightTime [10];
     int customerIndex;
     int nextFlightDay;
+    
+    printf(" 		(a) Press 0 to Exit.\n");
+	printf("		(b) Press 1 to Login as admin.\n");
+	printf("		(c) Press 2 to Register as admin.\n");
+	printf("		(d) Press 3 to Login as Passenger.\n");
+	printf("		(e) Press 4 to Register as Passenger.\n");
+	printf("		(f) Press 5 to Display the User Manual.\n");
+	printf("		Enter the desired option:     ");
     
 }
