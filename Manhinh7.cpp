8de848f996c2854bda5fb6575baef8c59a6c3114
vanhieu183 <levanhieu18032001@gmail.com>
2022-12-30 16:12:44 +0700

    Create Manhinh7.cpp

---
+++ Manhinh7.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	printf("Enter the desired Choice:	4\n");
+	
+	printf("\nd8888b. .d8b.  d8b   db d8888b.  .d88b.  .88b  d88.      d88888b db      d888888b  d888b  db   db d888888b      .d8888.  .o88b. db   db d88888b d8888b. db    db db      d88888b\n");
+	printf( "88  `8D d8' `8b 888o  88 88  `8D .8P  Y8. 88'YbdP`88      88'     88        `88'   88' Y8b 88   88 `~~88~~'      88'  YP d8P  Y8 88   88 88'     88  `8D 88    88 88      88'    \n");
+	printf( "88oobY' 88ooo88 88V8o 88 88   88 88    88 88  88  88      88ooo   88         88    88      88ooo88    88         `8bo.   8P      88ooo88 88ooooo 88   88 88    88 88      88ooooo\n");
+	printf( "88`8b   88~~~88 88 V8o88 88   88 88    88 88  88  88      88~~~   88         88    88  ooo 88~~~88    88           `Y8b. 8b      88~~~88 88~~~~~ 88   88 88    88 88      88~~~~~\n");
+	printf( "88 `88. 88   88 88  V888 88  .8D `8b  d8' 88  88  88      88      88booo.   .88.   88. ~8~ 88   88    88         db   8D Y8b  d8 88   88 88.     88  .8D 88b  d88 88booo. 88.    \n");
+	printf( "88   YD YP   YP VP   V8P Y8888D'  `Y88P'  YP  YP  YP      YP      Y88888P Y888888P  Y888P  YP   YP    YP         `8888Y'  `Y88P' YP   YP Y88888P Y8888D' ~Y8888P' Y88888P Y88888P\n");
+	printf("\nAre you thinking that it's not feasible(RandomSchedule)...Then you can confirm it by re-running the program...");
+	printf("\n\n");
+	printf("+------+-------------------------------+-----------+------------------+--------------------+------------------+------------------------------------+---------------------+--------+----------------------+\n");
+    printf("| Num  | FLIGHT SCHEDULE\t       | FLIGHT NO | Available Seats  |FROM ====>>         |====>> TO         |    ARRIVAL TIME\t\t\t   | FLIGHT TIME\t |  GATE  |   DISTANCE(MILES/KMS)|\n");
+    printf("+------+-------------------------------+-----------+------------------+--------------------+------------------+------------------------------------+---------------------+--------+----------------------+\n");
+}
